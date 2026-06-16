1ad790ec6df1a3a3213827a06d49920b17c389e9
unknown <ezewheel@gmail.com>
2026-06-16 03:36:20 -0300

    Aniadido bloqueo de acceso para usuarios de tipo no correspondiente

---
+++ config/requiere_ceo.php
@@ -0,0 +1,9 @@
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+if (!isset($_SESSION['codUsuario']) || ($_SESSION['tipoUsuario'] ?? '') !== 'ceo de aerolinea') {
+    header('Location: ../Flujo Sesion/login.php');
+    exit;
+}
